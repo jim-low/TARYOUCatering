b7da4847ff9221d9aba409425c1cf42184cc4850
jim-low <robil12380@gmail.com>
2022-08-23 13:44:00 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# TARYOUCatering
+Catering system for Data Structures and Algorithm College Assignment
+
+## Members
+Jim - Customer module
+Brian - Order module
+Jasper - Staff module
+Wen Wei - Payment module
---
+++ main/TARCatering.java
@@ -0,0 +1,16 @@
+package main;
+
+public class TARCatering {
+    public static void main(String[] args) {
+        mainBanner();
+    }
+
+    public static void mainBanner() {
+        System.out.println("  _________    ______  ______  __  ________      __            _");
+        System.out.println(" /_  __/   |  / __ \\ \\/ / __ \\/ / / / ____/___ _/ /____  _____(_)___  ____ _");
+        System.out.println("  / / / /| | / /_/ /\\  / / / / / / / /   / __ `/ __/ _ \\/ ___/ / __ \\/ __ `/");
+        System.out.println(" / / / ___ |/ _, _/ / / /_/ / /_/ / /___/ /_/ / /_/  __/ /  / / / / / /_/ /");
+        System.out.println("/_/ /_/  |_/_/ |_| /_/\\____/\\____/\\____/\\__,_/\\__/\\___/_/  /_/_/ /_/\\__, /");
+        System.out.println("                                                                   /____/");
+    }
+}
