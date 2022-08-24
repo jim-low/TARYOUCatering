4a4eafb588403fb3325e0bcbf3536cb585ccf601
jim-low <robil12380@gmail.com>
2022-08-24 17:58:09 +0800

    feat: add customer constructor

--- customer/Customer.java
+++ customer/Customer.java
@@ -1,9 +1,24 @@
 package customer;
 
+import general.Address;
 import general.Person;
 
 /**
  * Customer
  */
 public class Customer extends Person {
+    private Address savedAddress;
+
+    public Customer(String name, String email, String gender, String phoneNum, Address savedAddress) {
+        super(name, email, gender, phoneNum);
+        this.savedAddress = savedAddress;
+    }
+
+	public Address getSavedAddress() {
+		return savedAddress;
+	}
+
+	public void setSavedAddress(Address savedAddress) {
+		this.savedAddress = savedAddress;
+	}
 }
