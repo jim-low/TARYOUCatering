01acb227f909e9cdd8073947c5040b97d6ee2bcc
jim-low <robil12380@gmail.com>
2022-08-24 10:04:39 +0800

    feat: add constructor

--- customer/CircularLinkedList.java
+++ customer/CircularLinkedList.java
@@ -3,5 +3,12 @@ package customer;
 /**
  * CircularLinkedList
  */
-public class CircularLinkedList {
+public class CircularLinkedList<T> implements CircularLinkedListInterface<T> {
+    private T head;
+    private int size;
+
+    public CircularLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
 }
