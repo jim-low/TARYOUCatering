9da9c2a7972745bd778c9011f1bf55cbfa05fd4c
jim-low <robil12380@gmail.com>
2022-08-24 10:24:59 +0800

    feat: implement size method

--- customer/CircularLinkedList.java
+++ customer/CircularLinkedList.java
@@ -13,4 +13,39 @@ public class CircularLinkedList<T> implements CircularLinkedListInterface<T> {
         this.head = null;
         this.size = 0;
     }
+
+	@Override
+	public void insert(T data) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insert(T data, int index) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public T remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean search(T data) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		return this.size;
+	}
 }
