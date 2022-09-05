2595facb6b1b865c3488b487969559a3bcb85b5b
ZullyStunts <96515529+ZullyStunts@users.noreply.github.com>
2022-09-05 16:29:14 +0800

    Updated add feature in main ADT test

--- main/TARCatering.java
+++ main/TARCatering.java
@@ -56,14 +56,20 @@ public class TARCatering {
         System.out.println("2) Add Payment (WIP, only fixed obj payment atm)        ");
         System.out.println("3) Remove Payment (WIP, only with fixed obj atm)        ");
         System.out.println("4) Search Payment                                       ");
-        System.out.println("5) Exit Payment Menu                                    ");
+        System.out.println("5) Delete Payment List                                  ");
+        System.out.println("6) Exit Payment Menu                                    ");
         System.out.println("********************************************************");
         System.out.println("Enter a choice : ");
         choice = scan.nextInt();
         
             switch (choice) {
-                case 1:
+                case 1: //display
                     //display (using iterator to print)
+                    if (payList.isEmpty()){
+                        System.out.println("Your List is Empty...");
+                        break;
+                    }
+                    
                     while(payIterator.hasNext()){
                         Payment pay = payIterator.next();
                         date = dateFormat.format(pay.getPaymentDate());
@@ -80,11 +86,48 @@ public class TARCatering {
                     
  
                     break;
-                case 2:
-                    System.out.println("Not implemented yet.");
+                    
+                case 2: //add
+                    
+                    System.out.println("To test contains(), there will be no generation or validation for ID.");
+                    
+                    System.out.print("Enter the ID you want to add by : ");
+                    String addId = scan.nextLine() + scan.nextLine();
+                    
+                    System.out.print("\nEnter the Payment Amount : ");
+                    Double addAmt = scan.nextDouble();
+                    
+                    System.out.print("Enter the Date of the Payment : ");
+                    System.out.print("\nDay Of Payment : ");
+                    int day = scan.nextInt();
+                    System.out.print("\nMonth Of Payment : ");
+                    int month = scan.nextInt();
+                    System.out.print("\nYear Of Payment : ");
+                    int year = scan.nextInt();
+                    
+                    System.out.println("\n\nEnter the Payment Method :");
+                    String addMethod = scan.nextLine() + scan.nextLine();
+                    
+                    LocalDate dToAdd = LocalDate.of(year,month,day);
+                    Payment checkpay = new Payment(addId, addAmt, dToAdd, addMethod);
+                    
+                    if (!payList.contains(checkpay)){
+                        payList.add(checkpay);
+                        System.out.println("New payment added.");
+                    }
+                    
+                    else {
+                        System.out.println("This payment already exists.");
+                    }
+
                     break;
-                case 3:
-                    //remove
+                    
+                case 3: //remove
+                    if (payList.isEmpty()){
+                        System.out.println("You have nothing to delete, the list is empty...");
+                        break;
+                    }
+                    
                     Payment testRemove = new Payment("P0001", 3.33, d2, "MAYBANK");
                     if (!payList.remove(testRemove)){
                         System.out.println("No such record found! No changes made to List!");
@@ -93,8 +136,8 @@ public class TARCatering {
                         System.out.println("Record Deleted! ");
                     }   
                     break;
-                case 4:
-                    //search (contains()) 
+                    
+                case 4: //search
                     boolean found = false;
                     System.out.println("Enter the ID you want to search by : ");
                     String searchId = scan.nextLine() + scan.nextLine();
@@ -111,16 +154,24 @@ public class TARCatering {
                     
                     if (found == false) System.out.println("\n===No Such Record found...===");
                     break;
-                case 5:
+                    
+                case 5: //clear
+                        System.out.println("I don't know why you wanted to delete all the records, but ok boss.");
+                        payList.clear();
+                        System.out.println("PAYMENT LIST CLEARED.");
+                    break;
+                    
+                case 6://exit
                         System.out.println("Bye bye!");
   
                     break;
+                    
                 default:
-                    System.out.println("\nERROR: Please insert a number from 1 to 5.\n");
+                    System.out.println("\nERROR: Please insert a number from 1 to 6.\n");
                     break;
             }
         }
-        while (choice != 5);
+        while (choice < 6);
         
 
     }
