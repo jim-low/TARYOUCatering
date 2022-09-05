dbd90ab9384f1fce26caf6e54bc222dff1b1213f
ZullyStunts <96515529+ZullyStunts@users.noreply.github.com>
2022-09-05 15:44:32 +0800

    Added Payment entity and ADTs
    Changes:
    TARCatering.java : added a function to test Payment ADTs.
    Order.java : shud have ntg, idk what i changed
    Payment.java : Implemented everything except the variable from a required different ADT of Order object
    SortedLinkedList.java : All Functions should work. Only exception of a spesific condition error with remove();
    SortedListInterface.java : Should have implemented all required functions.

--- payment/Payment.java
+++ payment/Payment.java
@@ -1,7 +1,81 @@
 package payment;
 
-/**
- * Payment
- */
-public class Payment {
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
+public class Payment implements Comparable<Payment>{
+    
+    String paymentID;
+    double paymentAmt;
+    LocalDate paymentDate;
+    String paymentMethod;
+    //CircularLinkedList<Order> totalPayments; //assume this is an array.
+    
+    public Payment(String paymentID, double paymentAmt, LocalDate paymentDate, String paymentMethod) {
+    this.paymentID = paymentID;
+    this.paymentAmt = paymentAmt;
+    this.paymentDate = paymentDate;
+    this.paymentMethod = paymentMethod;
+    }
+
+    public void setPaymentID(String paymentID) {
+        this.paymentID = paymentID;
+    }
+
+    public void setPaymentAmt(double paymentAmt) {
+        this.paymentAmt = paymentAmt;
+    }
+
+    public void setPaymentDate(LocalDate paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getPaymentID() {
+        return paymentID;
+    }
+
+    public double getPaymentAmt() {
+        return paymentAmt;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    //do not add functions that receive input or display output.
+    
+    public Payment searchPayment(String id){
+        
+        if (this.paymentID.equals(id)){
+            Payment foundPayment = new Payment(this.paymentID, this.paymentAmt, this.paymentDate, this.paymentMethod);
+              //return the matched object
+              return foundPayment;
+        }
+   
+        else return null;
+        
+    }
+    
+    @Override
+    public String toString() {
+        return "paymentID = " + paymentID + ", paymentAmt = " + paymentAmt + ", paymentDate = " + paymentDate + ", paymentMethod = " + paymentMethod;
+    }
+
+    @Override
+    public int compareTo(Payment p) { //compare using ID, assuming (Letters)(Numbers)
+        String[] numToCompare = p.paymentID.split("[a-zA-Z]+");
+        String[] currentNum = this.paymentID.split("[a-zA-Z]+");
+        return Integer.parseInt(currentNum[1]) - Integer.parseInt(numToCompare[1]); 
+    }
+    
 }
+
+
