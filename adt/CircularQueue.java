306059a20f8ce4c230429ff9c208175ae388fcec
shiguree47 <jacknajarala@gmail.com>
2022-09-06 13:09:13 +0800

    Update CircularQueue Added CircularQueueInterface
    - Added CircularQueueInterface
    - Updated code in CircularQueue to implement general.node

--- adt/CircularQueue.java
+++ adt/CircularQueue.java
@@ -1,7 +1,71 @@
-package staff;
+package adt;
 
 /**
  * CircularQueue
  */
-public class CircularQueue {
+public class CircularQueue<T> implements CircularQueueInterface<T> {
+    private Node lastNode;
+    
+    public CircularQueue(){
+        lastNode = null;
+    }
+    
+    public void enqueue (T newEntry){
+        Node newNode = new Node (newEntry, null);
+        if(isEmpty()){
+            newNode.next = newNode;
+        }else{
+            newNode.next = lastNode.next;
+            lastNode.next = newNode;
+        }
+        lastNode = newNode;
+    }
+    
+    public T dequeue() {
+        T front = null;
+        if(!isEmpty()){
+            front = getFront();
+            if(lastNode.next == lastNode){
+                lastNode = null;
+            }else{
+                lastNode.next = lastNode.next.next;
+            }
+        }
+        
+        return front;
+    }
+
+    public T getFront() {
+        T frontData = null;
+        if(!isEmpty()){
+            frontData = lastNode.next.data;
+        }
+        return frontData;
+    }
+
+    public boolean isEmpty() {
+        return lastNode == null;
+    }
+    
+    public void clear(){
+        if(lastNode != null){
+            lastNode.next = null;
+        }
+        lastNode = null;
+    }
+    
+    private class Node{
+        private T data;
+        private Node next;
+        
+        public Node(T data){
+            this.data = null;
+            this.next = null;
+        }
+        
+        public Node (T data, Node anotherNode){
+            this.data = data;
+            this.next = anotherNode;
+        }
+    }
 }
---
+++ adt/CircularQueueInterface.java
@@ -0,0 +1,9 @@
+package adt;
+
+public interface CircularQueueInterface<T> {
+    public void enqueue(T newEntry);
+    public T dequeue();
+    public T getFront();
+    public boolean isEmpty();
+    public void clear();
+}
