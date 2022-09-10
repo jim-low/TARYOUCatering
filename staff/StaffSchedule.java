2e22cfbda5acafed6a7fefdaa66ec9236b3dc54b
jim-low <robil12380@gmail.com>
2022-09-11 01:45:13 +0800

    feat: nothing

--- staff/StaffSchedule.java
+++ staff/StaffSchedule.java
@@ -1,45 +1,44 @@
 package staff;
 
-import general.Person;
 import java.util.Date;
 
 public class StaffSchedule {
-    private Person userID;
+    private Staff userID;
     private Date time;
     private String day;
-    
+
     //Contructor for StaffSchedule
-    public StaffSchedule(Person userID, Date time, String day){
+    public StaffSchedule(Staff userID, Date time, String day){
         this.userID = userID;
         this.time = time;
         this.day = day;
     }
-    
+
     //Getter for userID
-    public Person getUserID(){
+    public Staff getUserID(){
         return userID;
     }
-    
+
     //Setter for userID
     public void setUserID(){
         this.userID = userID;
     }
-    
+
     //Getter for time
     public Date getTime(){
         return time;
     }
-    
+
     //Setter for time
     public void setTime(){
         this.time = time;
     }
-    
+
     //Getter for day
     public String getDay(){
         return day;
     }
-    
+
     //Setter for day
     public void setDay(){
         this.day = day;
